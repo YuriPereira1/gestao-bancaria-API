ee15db09ef8f6afb057583acf0ff8892597c755e
Yuri F Pereira <yuriferpereira@gmail.com>
2025-12-16 22:02:38 -0300

    Refatorado main, routes e schemas, deixando-os mordeno com Annotated e Fields.

--- app/main.py
+++ app/main.py
@@ -4,6 +4,16 @@ from .database import engine
 
 models.Base.metadata.create_all(engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Sistema Financeiro",
+    summary="Crie contas e faça transações",
+    description="API com dois endpoints, podendo criar, acessar informações e requisitar transferência.",
+    version="0.4",
+    openapi_tags=routes.tags_metadata,
+    contact={
+        "name": "Yuri F. Pereira",
+    },
+    license_info={"name": "MIT"},
+)
 
 app.include_router(routes.router)
